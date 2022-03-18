2c378573a4285c59310805ba42036a257280dcca
Kimura-Taiki <taiguimucum850@gmail.com>
2022-03-18 09:47:24 +0000

    5.サーバーサイド側の処理を実装する。

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ https://qiita.com/ren0826jam/items/77c9b980365bd4f5197c
 
 3.ビュー側の実装をする。
 
+4.ActionCable用のファイルを生成する。
+
+5.サーバーサイド側の処理を実装する。
+
--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,8 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    # ブロードキャスト用のストリーム名を設定
+    stream_from "room_channel"
   end
 
   def unsubscribed
@@ -8,5 +10,19 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak
+    # クライアントがメッセージを送信した時に実行される処理
+    message = Message.create!(content: data["message"])
+    ActionCable.server.broadcast(
+      "room_channel", { message: render_message(message) }
+    )
+  end
+
+  private
+
+  def render_message
+    ApplicationController.render(
+      partial: "messages/message",
+      locals: { message: message }
+    )
   end
 end
